2a37f6bf4b4797433eaa24111f35c9e45ccc5409
williamsetiawan65 <92424828+williamsetiawan65@users.noreply.github.com>
2021-12-02 00:00:09 +0700

    Create nilai.cs

---
+++ nilai.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDesktop
+{
+    public class nilai
+    {
+        public int nilaiA;
+        public int nilaiB;
+        public int hasil;
+        public String modehitung;
+    }
+}
